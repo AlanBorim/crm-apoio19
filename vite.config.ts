6a8aac653da626f95512129bc4143c5600ae511d
Alan Borim <alan.borim@grupopll.com.br>
2025-06-24 17:57:15 -0300

    ajuste no vite para roteamento correto

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/', // Isso garante que os caminhos sejam relativos à raiz
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
   server: {
     host: '0.0.0.0', // Permite acesso externo
     allowedHosts: ['5173-i1rxewkzxonsy60fq39fa-5e7ba351.manusvm.computer'],
